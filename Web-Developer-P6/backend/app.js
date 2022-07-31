2deab1d6e054581b2ddc991268d50b0d65c0af23
dieyneba <dieynebafofana@hotmail.fr>
2022-07-31 14:18:19 +0200

    first commit

---
+++ Web-Developer-P6/backend/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+const mongoose = require('mongoose');
+// zYVuGoDWOaP7ZbRk
+mongoose.connect('mongodb+srv://hot_takes:zYVuGoDWOaP7ZbRk@cluster0.grshl.mongodb.net/hot_takes?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use((req, res) => {
+   res.json({ message: 'Votre requête a bien été reçue !' }); 
+});
+
+module.exports = app;
